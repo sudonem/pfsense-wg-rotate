a7c5283a2f51967e6e83de0cf4240ccdb800859c
sudonem <sudonem@33volt.com>
2025-03-20 14:19:25 -0400

    Rotate firewall ingress port in sync with wg tunnel listen port rotation

--- wireguard_rotate.sh
+++ wireguard_rotate.sh
@@ -2,33 +2,56 @@
 #
 # Rotate the listenprot on a WireGuard tunnel
 # https://github.com/sudonem/pfsense-wg-rotate
-# Date: 2025-03-17
+# Date: 2025-03-20
+# TODO: Support performing rotation on multiple tunnels (tbd)
 
 # Fail fast & loud
 set -eu
 
 # Full path to the config file to modify
-# This is /conf/config.xml by default, but you should
-# test this script on your own system using a different file.
-# config_file=/conf/config.xml
+# This is /conf/config.xml by default.
+#config_file=/conf/config.xml
 config_file=[config file]
 
-# Define port range to cycle through
-# IMPORTANT: Because these ports must be open on your WAN interface
-# I strongly advise keeping this range as small as possible
-port_start="51820"
-port_end="51830"
+# Specify the name of the firewall port alias that will be
+# assigned to the WireGuard tunnel so that that the allowed
+# WAN ports may also be updated dynamically
+port_alias="[WIREGUARD PORT ALIAS]"
 
-# Generate new port number
-new_port="$(jot -r 1 $port_start $port_end)"
+# Specify the name of the WireGuard tunnel that must be rotated
+tunnel_id="tun_wg0"
 
-# Find previous listen port
-current_port="$(grep '<listenport>' $config_file | sed 's/[[:space:]]//g' | sed -E 's#</?listenport>##g')"
+# Define range of random listen ports
+range_start="51820"
+range_end="51830"
+
+# Backup config.xml file
+cp $config_file "$config_file.bak"
+
+# Identify current port and the matching config file line number
+current_port="$(grep $tunnel_id -A5 $config_file | grep '<listenport>' | sed 's/[[:space:]]//g' | sed -E 's#</?listenport>##g')"
+
+port_line="$(grep -n $tunnel_id -A5 $config_file | grep '<listenport>' | sed 's/-//' | awk '{print $1}')"
+
+alias_line="$(grep -n '<alias>' -A5 $config_file | grep "$port_alias" -A3 | grep '<address>' | sed 's/-//' | awk '{print $1}')"
+
+# Generate random port number & confirm its different than current port
+generate=1
+while [ "$generate" -eq 1 ]; do
+  new_port="$(jot -r 1 $range_start $range_end)"
+  if [ "$new_port" -ne "$current_port" ]; then
+    generate=0
+  fi
+done
 
 # Replace wireguard tunnel listen port with new port number
-sed -E -i .bak "s/<listenport>[0-9]{5}/<listenport>$new_port/g" "$config_file"
-logger -s -t wireguard "Tunnel listen port updated from $current_port to $new_port."
+sed -i '' "${port_line}s/<listenport>.*<\/listenport>/<listenport>${new_port}<\/listenport>/" "$config_file"
+sed -i '' "${alias_line}s/<address>.*<\/address>/<address>${new_port}<\/address>/" "$config_file"
+logger -s -t wireguard "Listen port for $tunnel_id updated from $current_port to $new_port."
+
+# Apply changes made to config.xml
+/usr/local/sbin/pfSsh.php playback upgradeconfig
 
 # Restart wireguard service via php shell
-pfSsh.php playback svc restart WireGuard
+/usr/local/sbin/pfSsh.php playback svc restart WireGuard
 logger -s -t wireguard "Restarting wireguard tunnels."
